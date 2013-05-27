680361d041202f38febf362d4f6920f2651ff4c3
Michał Maciej Różański <michal.rozanski@gmail.com>
2013-05-27 15:02:52 +0200

    working import and export, but need some tweeks

--- ui/mainwindow.py
+++ ui/mainwindow.py
@@ -1,3 +1,12 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'ui/XonStat.ui'
+#
+# Created: Mon May 27 14:54:21 2013
+#      by: PyQt4 UI code generator 4.10.1
+#
+# WARNING! All changes made in this file will be lost!
+
 from PyQt4 import QtCore, QtGui
 
 try:
@@ -25,11 +34,11 @@ class Ui_MainWindow(object):
         MainWindow.setSizePolicy(sizePolicy)
         MainWindow.setMaximumSize(QtCore.QSize(10000, 10000))
         MainWindow.setWindowTitle(_fromUtf8("XonStat Tool"))
+        MainWindow.setLocale(QtCore.QLocale(QtCore.QLocale.English, QtCore.QLocale.UnitedStates))
         self.centralwidget = QtGui.QWidget(MainWindow)
         self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
         self.btnImport = QtGui.QPushButton(self.centralwidget)
         self.btnImport.setGeometry(QtCore.QRect(390, 30, 99, 23))
-        self.btnImport.setLocale(QtCore.QLocale(QtCore.QLocale.English, QtCore.QLocale.UnitedStates))
         self.btnImport.setObjectName(_fromUtf8("btnImport"))
         self.splitter = QtGui.QSplitter(self.centralwidget)
         self.splitter.setGeometry(QtCore.QRect(10, 10, 371, 36))
@@ -40,6 +49,19 @@ class Ui_MainWindow(object):
         self.lbImport.setObjectName(_fromUtf8("lbImport"))
         self.lineImport = QtGui.QLineEdit(self.splitter)
         self.lineImport.setObjectName(_fromUtf8("lineImport"))
+        self.splitter_2 = QtGui.QSplitter(self.centralwidget)
+        self.splitter_2.setGeometry(QtCore.QRect(10, 70, 371, 36))
+        self.splitter_2.setOrientation(QtCore.Qt.Vertical)
+        self.splitter_2.setObjectName(_fromUtf8("splitter_2"))
+        self.lbExport = QtGui.QLabel(self.splitter_2)
+        self.lbExport.setTextFormat(QtCore.Qt.AutoText)
+        self.lbExport.setObjectName(_fromUtf8("lbExport"))
+        self.lineExport = QtGui.QLineEdit(self.splitter_2)
+        self.lineExport.setObjectName(_fromUtf8("lineExport"))
+        self.btnExport = QtGui.QPushButton(self.centralwidget)
+        self.btnExport.setGeometry(QtCore.QRect(390, 90, 99, 23))
+        self.btnExport.setLocale(QtCore.QLocale(QtCore.QLocale.English, QtCore.QLocale.UnitedStates))
+        self.btnExport.setObjectName(_fromUtf8("btnExport"))
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtGui.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 500, 20))
@@ -55,3 +77,6 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         self.btnImport.setText(_translate("MainWindow", "Import", None))
         self.lbImport.setText(_translate("MainWindow", "Choose file to import", None))
+        self.lbExport.setText(_translate("MainWindow", "Choose file to export", None))
+        self.btnExport.setText(_translate("MainWindow", "Export", None))
+
--- xstool.py
+++ xstool.py
@@ -1,16 +1,37 @@
 import sys
+import os
+
 from PyQt4 import QtCore, QtGui
 
 from ui.mainwindow import Ui_MainWindow
-from fileutils import file_import
 
 class XonStatForm(QtGui.QMainWindow):
     def __init__(self, parent=None):
         QtGui.QWidget.__init__(self, parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.keypath = os.path.expanduser('~/.xonotic/key_0.d0si')
                 
-        QtCore.QObject.connect(self.ui.btnImport, QtCore.SIGNAL("clicked()"), file_import(self.ui.lineImport.text()))
+        QtCore.QObject.connect(self.ui.btnImport, QtCore.SIGNAL("clicked()"), self.file_import)
+        QtCore.QObject.connect(self.ui.btnExport, QtCore.SIGNAL("clicked()"), self.file_export)
+        
+    def file_import(self):
+        sourcepath = self.ui.lineImport.text()
+        
+        with open(sourcepath, 'rb') as sourcefile:
+            cache = sourcefile.read()
+        with open(self.keypath, 'wb') as keyfile:
+            keyfile.write(cache)
+
+    def file_export(self):
+        descpath = self.ui.lineExport.text()
+        
+        with open(self.keypath, 'rb') as keyfile:
+            cache = keyfile.read()
+        with open(descpath, 'wb') as descfile:
+            descfile.write(cache)
+
+        
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
